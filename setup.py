46f092626b603899abe69e03fcbf2b667f0a6b4e
Tasnimul Arabi Anik <arabianik987@gmail.com>
2025-04-01 17:32:26 +0600

    setup.py updated with python version and matplotlib-base

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fetchM",
-    version="0.1.1",
+    version="0.1.2",
     author="Tasnimul Arabi Anik",
     author_email="arabianik987@gmail.com",
     description="A Python tool for fetching metadata for bacterial genomes.",
